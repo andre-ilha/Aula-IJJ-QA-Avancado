56c79663654a5bf4d8600d9726b5e0191fd92c1a
Dehco <alveslimaandre@gmail.com>
2024-06-13 21:28:42 -0300

    acabou a aula

---
+++ Aula13-06/Desafio_Squad2.py
@@ -0,0 +1,18 @@
+#Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+nome = "Paula Martins "
+mes = "Janeiro"
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+format_1 = f"Olá {nome}. Em {mes} você realizou uma compra no valor de R${valor} e ganhou um desconto de {desconto}% em sua próxima compra. Use o cupom {cupom}."
+#format_2 = "Olá {}. Em {} você realizou uma compra no valor de R${} e ganhou um desconto de {}% em sua próxima compra. Use o cupom {}.".format(nome, mes, valor, desconto, cupom)
+#format_3 = "Olá {a}. Em {b} você realizou uma compra no valor de R${c} e ganhou um desconto de {d}% em sua próxima compra. Use o cupom {e}.".format(a=nome, b=mes, c=valor, d=desconto, e=cupom)
+
+print ("Olá " , nome , ". Em " , mes , " voce realizou uma compra no valor de R$" , valor , " e ganhou um desconto de " , desconto , "% em sua proxima compra. Use o cumpom" , cupom ,".")
+
+print = (format_1)
+#print = (format_2)
+#print = (format_3)
+
+#python Aula13-06\Desafio_Squad.py para rodar o codigo no terminal direto
---
+++ Aula13-06/input.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+int()
+
+#print(nome)
+print(f"O nome da pessoa é {nome}, e a idade é {idade}")
