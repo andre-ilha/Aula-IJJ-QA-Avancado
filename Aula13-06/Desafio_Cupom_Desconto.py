9fcf7f3c0a05296c2b1a74f8e66af63a5a8ad245
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-17 15:11:37 -0300

    Add files via upload

---
+++ Aula13-06/Desafio_Cupom_Desconto.py
@@ -0,0 +1,10 @@
+#Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+nome = "Paula Martins "
+mes= "Janeiro"
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+print ("Olá " , nome , ". Em " , mes , " voce realizou uma compra no valor de R$" , valor , " e ganhou um desconto de " , desconto , "% em sua proxima compra. Use o cumpom" , cupom ,".")
+
+#python Aula13-06\Desafio_Squad.py para rodar o codigo no terminal direto
