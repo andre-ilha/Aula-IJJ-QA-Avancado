23753f9ff209e224fbabf04953910be1a808937c
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-17 19:07:25 -0300

    att

--- Aula13-06/Desafios_Atividades/Desafio_Nome_Altura.py
+++ Aula13-06/Desafios_Atividades/Desafio_Nome_Altura.py
@@ -1,6 +1,8 @@
 nome = input("Por favor, digite seu nome de usuário: ")
 altura = input("Agora sua altura: ")
+idade = input("E por fim, escreva sua idade: ")
 
 alt = float(altura)
+ida = int(idade)
 
-print(f"O nome de usuário da pessoa é {nome}, e sua altura é {altura}")
+print(f"O nome de usuário da pessoa é {nome}, sua altura é {altura} e sua idade é {ida}")
