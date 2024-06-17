1b7a118da3538e7a38f81efd178a4d29f4d9ef44
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-17 15:54:02 -0300

    Entrega da atividades e organizacao de pastas

--- Aula13-06/Desafio_Nome_Altura.py
+++
@@ -1,6 +0,0 @@
-nome = input("Por favor, digite seu nome de usuário: ")
-altura = input("Agora sua altura: ")
-
-alt = float(altura)
-
-print(f"O nome de usuário da pessoa é {nome}, e sua altura é {altura}")
