95f0ec76616ba9f32e4dced87b82bbfc9c154058
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-17 21:38:55 -0300

    Fim da aula

---
+++ Aula17-06/Collections.py
@@ -0,0 +1,24 @@
+frutas = ['Manga', 'Maracuja', 'Uva'] #lista
+
+print (frutas)
+
+frutas.append('Melancia') # Adiciona 1 elemento no final da lista
+
+print (frutas)
+
+print (frutas[0])
+print (frutas[1])
+print (frutas[2])
+print (frutas[3])
+
+comidas = ('Hamburguer', 'Pizza', 'Feijoada') #tupla
+print(comidas)
+
+facilitadores = {"vini", 'Matheus', 'Michele'} #Sets
+print(facilitadores)
+
+dicionario = {
+    'nome': "vini",
+    'idade': 25
+}
+print(dicionario(Nome))
--- Aula17-06/OperadoresAritmeticos.py
+++ Aula17-06/OperadoresAritmeticos.py
@@ -1,3 +1,4 @@
+"""
 n1 = 5
 n2 = 3
 n3 = 4
@@ -7,3 +8,28 @@ soma = n1 + n2 + n3 + n4
 media = soma / 4
 
 print(media)
+
+"""
+
+
+"""
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("É maior de idade")15
+    
+"""
+
+"""
+cidade = "salvador"
+idade = 18
+
+if cidade == "salvador" and idade >= 18:
+    print(Salvador e maior)
+
+"""    
+##cidade = "salvador"
+##idade = 18
+
+##if "a" in cidade
+##    print(f"{cidade} possui a letra a")
