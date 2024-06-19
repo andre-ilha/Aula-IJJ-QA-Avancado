68a6c08913b16483799744d05766f7b326a0580a
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-18 21:38:45 -0300

    faltou uma parte!

--- Aula18-06/EstruturasCondicionais.py
+++ Aula18-06/EstruturasCondicionais.py
@@ -1,8 +1,20 @@
 time = input("Digite seu time: ")
 
-if time == 'Corinthians':
-    print("Você é um Timão!")
-elif time == "Bahia":               #Pode repetir o elif sempre que quiser colocar mais condições;
-    print("Você é Esquadrão")
-else:
-    print("Você não é Timão!")
+#if time == 'Corinthians':
+#    print("Você é um Timão!")
+#elif time == "Bahia":               #Pode repetir o elif sempre que quiser colocar mais condições;
+#    print("Você é Esquadrão")
+#else:
+#    print("Você não é Timão!")
+
+
+
+match time:
+    case "Corinthians":
+        print("Você é um Timão!")
+    case "Bahia":
+        print("Você é Esquadrão!")
+    case "Gremio":
+        print("Você é um Imortal!")
+    case _:
+        print("Quem não é, não se mete!")
