b02ae1c1bbe3a13d95d9d1dc9a1c8e112d002679
Dehco <alveslimaandre@gmail.com>
2024-06-26 21:22:53 -0300

    Atualizacao das Aulas

---
+++ Aula04_Operadores_Aritmeticos/Lousa01.py
@@ -0,0 +1,40 @@
+#Operadores Aritméticos
+
+#Adição  /  +
+#Subtração  /  -
+#Multiplicação  /  *
+#Divisão  /  /
+#Potência  /  **
+#Resto da Divisão  /  %
+#Divisão Inteira  /  //
+
+#Raiz Quadrada  /  9 ** (1/2)
+#Raiz Cúbica  /  9 ** (1/3)
+#Porcentagem  /  (valor_produto * (percentual_desconto / 100))
+
+
+#Operador   Exemplo     Equivale a
+#   =       x = 1         x = 1
+#  +=       x += 1       x = x + 1
+#  -=       x -= 1       x = x - 1
+#  *=       x *= 1       x = x * 1
+#  /=       x /= 1       x = x / 1
+#  %=       x %= 1       x = x % 1
+
+#Operador      Conceito      Exemplo
+#   >          Maior que      X > 5
+#   <          Menos que      x < 5
+#  ==          Igual          x == 5
+#  !=          Diferente      x != 5
+#  >=          Maior Igual    x >= 5
+#  <=          Menor Igual    x <= 5
+
+#Lógico
+#and  /  Retorna True se for verdadeiro  /   x > 1 and x < 5
+#or
+#not
+
+#Associoação
+#in
+#not in
+
