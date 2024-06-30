a749ee64eee8ca768df048896e3bb27f992541eb
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-30 19:10:24 -0300

    Envio de Atividade Desafio do Caique Biblioteca OS

---
+++ Aula07_EvitandoProblemas/DesafioCaique_BibliotecaOS.py
@@ -0,0 +1,27 @@
+import os #importante a biblioteca do sistema operacional
+
+#Criando uma lista de dados ficticia
+
+dados = [
+    {'nome': 'Andre', 'idade' : 39, 'cidade' : 'Ilhabela'},
+    {'nome': 'Alice', 'idade' : 31, 'cidade' : 'Sao Sebastiao'},
+    {'nome': 'Gustavo', 'idade' : 38, 'cidade' : 'Sao Bernardo do Campo'},
+    {'nome': 'Gabriel', 'idade' : 40, 'cidade' : 'Florianopolis'}
+]
+
+#Diretório onde o arquivo será salvo
+diretorio = 'DesafioCaique_BibliotecaOS'
+
+#Cria a pasta se ela não exitir
+if not os.path.exists(diretorio):
+    os.makedirs(diretorio)
+
+#Caminho do arquivo
+caminho_arquivo = os.path.join(diretorio, "DesafioCaique_BibliotecaOS.txt")
+
+#Escreve os dados no arquivo
+with open(caminho_arquivo, "w") as arquivo:
+    for pessoa in dados:
+        arquivo.write(f'Nome: {pessoa["nome"]}, Idade: {pessoa['idade']}, Cidade: {pessoa['cidade']}\n')
+
+print(f'Arquivo salvo em: {caminho_arquivo}')
