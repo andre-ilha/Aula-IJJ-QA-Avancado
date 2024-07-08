a8354eb262c10ff1e7da0b5671e8de1f754ce8b6
André Alves de Lima <alveslimaandre@gmail.com>
2024-07-08 19:58:57 -0300

    Atualização com correções do professor

--- DesafioFinal_ModuloPython/Main.py
+++ DesafioFinal_ModuloPython/Main.py
@@ -8,7 +8,7 @@ import json
 
 fake = Faker('pt_BR')
 
-def criarpersona() -> dict:
+def criar_persona() -> dict:
 
     data = {
         "username" : fake.user_name(),
@@ -20,28 +20,28 @@ def criarpersona() -> dict:
     }
     return data
 
-def salvar_txt(logjson: str, conteudo: str):
+def salvar_json(logjson: str, conteudo: str):
     with open(logjson, 'w') as arquivo:
         arquivo.write(conteudo + '\n')
     print(f'Dados salvo em {logjson}.')
 
-def criaruserapi() -> bool:
+def criar_user_api() -> bool:
     endpoint_criaruser = "https://desafiopython.jogajuntoinstituto.org/api/users/"
     response = requests.post(endpoint_criaruser, json=usuario)
 
     if response.status_code == 201:
         print('Usuário criado com sucesso!')
         print(response.json())
-        salvar_txt('log_criacao.txt', json.dumps(response.json(), indent=4))
+        salvar_json('log_criacao.json', json.dumps(response.json(), indent=4))
         return True
     else:
         print('Falha ao criar usuário.')
         print('Status code: ', response.status_code)
         print('Resposta: ', response.json())
-        salvar_txt('log_login.txt', json.dumps(response.json(), indent=4))
+        salvar_json('log_criacao.json', json.dumps(response.json(), indent=4))
         return False
 
-def loginuserapi():
+def login_user_api():
     endpoint_login = 'http://desafiopython.jogajuntoinstituto.org/api/users/login/'
     login_data = {
         'email': usuario["email"],
@@ -53,13 +53,13 @@ def loginuserapi():
     if response.status_code == 200:
         print('Login realizado com sucesso!')
         print(response.json())
-        salvar_txt('reposta_api.txt', json.dumps(response.json(), indent=4))
+        salvar_json('reposta_api.json', json.dumps(response.json(), indent=4))
     else:
         print('Falha ao realizar login.')
         print('Status code:', response.status_code)
         print('Resposta:', response.json())
-        salvar_txt('reposta_api.txt', json.dumps(response.json(), indent=4))
+        salvar_json('reposta_api.json', json.dumps(response.json(), indent=4))
 
-usuario = criarpersona()
-if criaruserapi():
-     loginuserapi()
+usuario = criar_persona()
+if criar_user_api():
+     login_user_api()
